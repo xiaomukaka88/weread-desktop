3207ff901def8b48d4d2b5ddf58909b3a657b3af
TechFetch <user@techfetch.dev>
2026-05-22 09:07:16 +0800

    fix: add channel allow-lists to preload IPC bridge

--- preload/index.js
+++ preload/index.js
@@ -1,16 +1,37 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("weread", {
-  // IPC send
-  send: (channel, data) => ipcRenderer.send(channel, data),
-  invoke: (channel, data) => ipcRenderer.invoke(channel, data),
+const ALLOWED_SEND_CHANNELS = new Set(["open-settings", "open-stats"]);
+const ALLOWED_INVOKE_CHANNELS = new Set([
+  "get-users", "save-users", "get-active-user", "set-active-user",
+  "add-user", "remove-user", "start-reading", "stop-reading",
+  "get-session-status", "get-stats", "get-daily-stats",
+]);
+const ALLOWED_LISTEN_CHANNELS = new Set(["session:update", "log"]);
 
-  // IPC receive
-  on: (channel, callback) => {
+function filterSend(channel, data) {
+  if (ALLOWED_SEND_CHANNELS.has(channel)) {
+    ipcRenderer.send(channel, data);
+  }
+}
+
+function filterInvoke(channel, data) {
+  if (ALLOWED_INVOKE_CHANNELS.has(channel)) {
+    return ipcRenderer.invoke(channel, data);
+  }
+}
+
+function filterOn(channel, callback) {
+  if (ALLOWED_LISTEN_CHANNELS.has(channel)) {
     const handler = (_, ...args) => callback(...args);
     ipcRenderer.on(channel, handler);
     return () => ipcRenderer.removeListener(channel, handler);
-  },
+  }
+}
+
+contextBridge.exposeInMainWorld("weread", {
+  send: filterSend,
+  invoke: filterInvoke,
+  on: filterOn,
 
   onSessionUpdate: (callback) => {
     const handler = (_, data) => callback(data);
@@ -24,7 +45,6 @@ contextBridge.exposeInMainWorld("weread", {
     return () => ipcRenderer.removeListener("log", handler);
   },
 
-  // Specific invoke helpers
   getUsers: () => ipcRenderer.invoke("get-users"),
   saveUsers: (data) => ipcRenderer.invoke("save-users", data),
   getActiveUser: () => ipcRenderer.invoke("get-active-user"),
